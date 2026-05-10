bb93b72dab66e3434fec659d47f0658a743ef288
Tarik <tarikkuet@gmail.com>
2026-05-10 22:37:01 +0600

    fixed config

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Tell Flask exactly where the database is
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'dse_scanner.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dse_scanner.db')
 
 class Config:
     # A secret key is required by Flask to secure user sessions for your Admin panel
     SECRET_KEY = 'super-secret-trading-key'
     
     # Tells SQLAlchemy exactly where to create your SQLite database file
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dse_scanner.db')
     
     # Turns off a feature we don't need to save memory
     SQLALCHEMY_TRACK_MODIFICATIONS = False
