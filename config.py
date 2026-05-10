f4aaccb19c8f4c1f4f9cb65971b5827cd242f466
Tarik <tarikkuet@gmail.com>
2026-05-10 21:03:56 +0600

    Fixed screener modal, added native search dropdown, updated absolute path for db

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 # Gets the absolute path of your current directory
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Tell Flask exactly where the database is
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'dse_scanner.db')
+
 class Config:
     # A secret key is required by Flask to secure user sessions for your Admin panel
     SECRET_KEY = 'super-secret-trading-key'
